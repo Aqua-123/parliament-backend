a2a2d5c9759f49a01a58b7f84eb90fa528eb7c60
Aqua <86239876+Aqua-123@users.noreply.github.com>
2024-04-17 00:47:04 +0530

    push

--- API/Modals/User.js
+++ API/Modals/User.js
@@ -5,17 +5,16 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   name: { type: String, required: true },
   constituency: { type: String, required: true },
-  sabha: { type: String, required: true },
-  mothertongue: { type: String, required: true },
+  house: { type: String, required: true },
+  motherTongue: { type: String, required: true },
   password: { type: String, required: true },
 });
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
-    this.password = bcrypt.hash(this.password, 8);
+    this.password = await bcrypt.hash(this.password, 8);
   }
   next();
 });
-
 const User = mongoose.model("User", userSchema);
 export default User;
--- API/server.js
+++ API/server.js
@@ -8,159 +8,197 @@ import TranslateTextAzure from "./src/Translate.js";
 import TextToSpeechAzure from "./src/TTS.js";
 import SpeechToText from "./src/ASR.js";
 import User from "./Modals/User.js";
+import http from "http";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const TOTAL_CPUS = os.cpus().length;
-const DEBUG_MODE = true;
+import { Server } from "socket.io";
 
-const storage = multer.memoryStorage(); // Stores files in memory
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+import mongoose from "mongoose";
 
-if (cluster.isPrimary) {
-  console.log(`Master ${process.pid} is running`);
+const mongoURI = "mongodb://localhost:27017/parliament";
 
-  const numWorkers = DEBUG_MODE ? 1 : TOTAL_CPUS;
-  for (let i = 0; i < numWorkers; i++) {
-    cluster.fork();
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Optional: In a production environment, you should handle process termination gracefully
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("MongoDB connection disconnected due to app termination");
+    process.exit(0);
+  });
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const client = createClient({ url: "redis://localhost:6379" });
+
+client.on("connect", () => console.log("Connected to Redis"));
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+app.get("/", async (req, res) => {
+  const cacheKey = "home";
+  const cachedData = await client.get(cacheKey);
+
+  if (cachedData) {
+    console.log("Cache hit");
+    return res.send(`Cached: ${cachedData}`);
+  } else {
+    console.log("Cache miss");
+    const freshData = "Hello World from Express!";
+    await client.set(cacheKey, freshData, {
+      EX: 600,
+    });
+    return res.send(freshData);
+  }
+});
+
+app.get("/translate", async (req, res) => {
+  const { text, lang } = req.query;
+  if (!text || !lang) return res.status(400).send("Invalid request");
+
+  const translation = await TranslateTextAzure(text, lang);
+  if (!translation) return res.status(500).send("Failed to translate text");
+
+  return res.send(translation);
+});
+
+app.get("/tts", async (req, res) => {
+  const { text, gender, lang } = req.query;
+  if (!text || !gender || !lang) return res.status(400).send("Invalid request");
+
+  const audio = await TextToSpeechAzure(text, lang, gender);
+
+  if (!audio) return res.status(500).send("Failed to convert text to speech");
+
+  res.setHeader("Content-Type", "audio/wav");
+  return res.send(audio);
+});
+
+app.post("/asr", upload.single("audio"), async (req, res) => {
+  console.log("Received audio file");
+
+  if (!req.file || !req.body.lang) {
+    return res
+      .status(400)
+      .send("Invalid request: No file or language provided");
   }
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  const app = express();
-  app.use(cors()); // Enable CORS for all routes and origins
-  const client = createClient({
-    url: "redis://localhost:6379",
-  });
+  const { lang } = req.body;
+  const audioBuffer = req.file.buffer;
 
-  client.on("connect", () => console.log("Connected to Redis"));
-  client.on("error", (err) => console.log("Redis Client Error", err));
-
-  app.get("/", async (req, res) => {
-    const cacheKey = "home";
-    const cachedData = await client.get(cacheKey);
-
-    if (cachedData) {
-      console.log("Cache hit");
-      return res.send(`Cached: ${cachedData}`);
-    } else {
-      console.log("Cache miss");
-      const freshData = "Hello World from Express!";
-      await client.set(cacheKey, freshData, {
-        EX: 600,
-      });
-      return res.send(freshData);
+  try {
+    const text = await SpeechToText(audioBuffer, lang);
+    if (!text) {
+      return res.status(500).send("Failed to convert speech to text");
     }
-  });
+    return res.send({ text });
+  } catch (error) {
+    console.error("Error processing the audio:", error);
+    return res.status(500).send("Server error");
+  }
+});
 
-  app.get("/translate", async (req, res) => {
-    const { text, lang } = req.query;
-    if (!text || !lang) return res.status(400).send("Invalid request");
+app.post("/sign-up", async (req, res) => {
+  try {
+    console.log(req.body);
+    console.log(req.headers);
+    const { email, name, constituency, house, motherTongue, password } =
+      req.body;
 
-    const translation = await TranslateTextAzure(text, lang);
-    if (!translation) return res.status(500).send("Failed to translate text");
+    const user = new User({
+      email,
+      name,
+      constituency,
+      house,
+      motherTongue,
+      password,
+    });
 
-    return res.send(translation);
-  });
+    await user.save();
 
-  app.get("/tts", async (req, res) => {
-    const { text, gender, lang } = req.query;
-    if (!text || !gender || !lang)
-      return res.status(400).send("Invalid request");
+    const token = jwt.sign({ id: user._id }, "secretKey", {
+      expiresIn: "12h",
+    });
+    res.status(201).json({ token, user });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-    const audio = await TextToSpeechAzure(text, lang, gender);
-
-    if (!audio) return res.status(500).send("Failed to convert text to speech");
-
-    res.setHeader("Content-Type", "audio/wav");
-    return res.send(audio);
-  });
-
-  app.post("/asr", upload.single("audio"), async (req, res) => {
-    console.log("Received audio file");
-
-    if (!req.file || !req.body.lang) {
-      return res
-        .status(400)
-        .send("Invalid request: No file or language provided");
+app.post("/sign-in", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).send("Authentication failed");
     }
+    const token = jwt.sign({ id: user._id }, "secretKey", {
+      expiresIn: "12h",
+    });
+    res.json({ token, user });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-    const { lang } = req.body;
-    const audioBuffer = req.file.buffer;
+const verifyToken = (req, res, next) => {
+  if (DEBUG) {
+    req.user = { id: "testuser" };
+    return next();
+  }
+  let token = req.header("Authorization");
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
+  if (!token) return res.status(401).send("Access denied");
+  try {
+    const verified = jwt.verify(token, "secretKey");
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
 
-    try {
-      const text = await SpeechToText(audioBuffer, lang);
-      if (!text) {
-        return res.status(500).send("Failed to convert speech to text");
-      }
-      return res.send({ text });
-    } catch (error) {
-      console.error("Error processing the audio:", error);
-      return res.status(500).send("Server error");
-    }
+app.get("/user", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+//  SOCKET LOGIC STARTS HERE
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", credentials: true },
+});
+
+const port = 4000;
+const userList = [];
+
+io.on("connection", (socket) => {
+  socket.on("join", (userId) => {
+    if (!userList.includes(userId)) userList.push(userId);
+
+    socket.join(userId);
+    socket.emit("user-list", userList);
   });
-
-  app.post("/sign-up", async (req, res) => {
-    try {
-      const { email, name, constituency, sabha, mothertongue, password } =
-        req.body;
-
-      const user = new User({
-        email,
-        name,
-        constituency,
-        sabha,
-        mothertongue,
-        password,
-      });
-
-      await user.save();
-
-      const token = jwt.sign({ id: user._id }, "secretKey", {
-        expiresIn: "16",
-      });
-      res.status(201).json({ token, userType });
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
+  socket.on("text", (data) => {
+    const { username, room, message, gender, data: translated } = data;
+    io.to(room).emit("response", { username, message, gender, translated });
   });
+});
 
-  app.post("/sign-in", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).send("Authentication failed");
-      }
-      const token = jwt.sign({ id: user._id }, "secretKey", {
-        expiresIn: "12h",
-      });
-      res.json({ token, user });
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  });
-
-  // const verifyToken = (req, res, next) => {
-  //   if (DEBUG) {
-  //     req.user = { id: "testuser" };
-  //     return next();
-  //   }
-  //   let token = req.header("Authorization");
-  //   if (token.startsWith("Bearer ")) {
-  //     token = token.slice(7, token.length);
-  //   }
-  //   if (!token) return res.status(401).send("Access denied");
-  //   try {
-  //     const verified = jwt.verify(token, "secretKey");
-  //     req.user = verified;
-  //     next();
-  //   } catch (error) {
-  //     res.status(400).send("Invalid token");
-  //   }
-  // };
-
-  app.listen(4000, () => {
-    console.log(`Worker ${process.pid} started`);
-  });
-}
+server.listen(port, () => {
+  console.log(`Worker ${process.pid} started`);
+});
